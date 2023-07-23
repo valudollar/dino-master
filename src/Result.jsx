27bd1935d6eda1a380131372972ed0821ac34991
valudollar <valerie.luxuanhui@gmail.com>
2023-07-23 18:34:44 +0800

    conditional display title gif

--- src/Result.jsx
+++ src/Result.jsx
@@ -1,6 +1,9 @@
 import { useState, useEffect } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import "./App.css";
+import stego from "./assets/stego.gif";
+import trike from "./assets/trike.gif";
+import pete from "./assets/pete.gif";
 
 function Result() {
   const location = useLocation();
@@ -9,6 +12,7 @@ function Result() {
   const [percent, setPercent] = useState(0);
   const [comment, setComment] = useState("");
   const [title, setTitle] = useState("");
+  const [src, setSrc] = useState("");
 
   function getPercentScore() {
     const dec = (finalScore * 100) / totalQns;
@@ -21,18 +25,23 @@ function Result() {
     if (result === 100) {
       setComment("You are worthy...");
       setTitle("Legendary Dino Master");
+      setSrc(trike);
     } else if (result < 100 && result > 80) {
       setComment("Aw, you almost made it!");
       setTitle("Dino Expert");
+      setSrc(trike);
     } else if (result <= 80 && result > 50) {
       setComment("Not bad, but how many were guesses?");
       setTitle("Dino Apprentice");
+      setSrc(trike);
     } else if (result <= 50 && result > 0) {
       setComment("Did you even try...?");
       setTitle("Dino Noob");
+      setSrc(trike);
     } else if (result === 0) {
       setComment("Damn...");
       setTitle("Just an idiot");
+      setSrc(stego);
     }
     console.log(comment);
     console.log(title);
@@ -48,13 +57,14 @@ function Result() {
         <header className="">
           <h1>DINO MASTER</h1>
         </header>
-        <section>
-          <h4>
+        <section className="resultSection">
+          <h2>
             Your final score is {finalScore} out of {totalQns}
-          </h4>
-          <h4>You got {percent}% of the questions correct!</h4>
-          <h4>{comment}</h4>
-          <h4>Title: {title}</h4>
+          </h2>
+          <h3>You got {percent}% of the questions correct!</h3>
+          <h2>{comment}</h2>
+          <h3>Title: {title}</h3>
+          <img className="titlecontainer" src={src} onClick={check}></img>
           <a href="/">
             <button className="button">Try Again</button>
           </a>
